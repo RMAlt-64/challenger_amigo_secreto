c28e26ffa3f19e68a81a5cc01a37afba6fae42f0
RMAlt-64 <ruben.manuel.almiron@gmail.com>
2025-03-12 14:26:43 -0300

    Haciendo la logica, para las funciones del juego

---
+++ app.js
@@ -0,0 +1,3 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let input = document.getElementById('amigo');
+console.log(input);
