b18c7cfdca9b3b04ab7fd80b61d841d164648051
RMAlt-64 <ruben.manuel.almiron@gmail.com>
2025-03-12 14:49:01 -0300

    Vamos a realizar la logica de las funciones del juego

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let input = document.getElementById('amigo');
 console.log(input);
+addEventListener('click', agregarAmigo());
