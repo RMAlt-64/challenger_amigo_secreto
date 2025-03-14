50de83263c1c4e88884be2319ac4fcf01591a0fe
RMAlt-64 <ruben.manuel.almiron@gmail.com>
2025-03-14 00:48:11 -0300

    Se termino las funciones y se hicieron personalizo la paginas

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
-
 let entrada = document.getElementById('amigo');
-let boton = document.getElementById('boton');
 let listaAmigos = [];
 function agregarAmigo(){
-    if(entrada.value == ""){
-        alert("Debe ingresar un nombre");
-    } else {
-        let lista = document.getElementById('listaAmigos');
+    if(entrada.value !== ""){
         let nombres = entrada.value;
         listaAmigos.push(nombres);
-        console.log(listaAmigos);
-        let li = document.createElement('li');
-        li.textContent = nombres;
-        lista.appendChild(li);
         entrada.value = "";
+        actualizarLista();
+    } else {
+        alert("Por favor, inserte un nombre.");
     }
-    
 }
+
+function actualizarLista(){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+    for(let i = 0; i <listaAmigos.length; i++){
+        let li = document.createElement("li");
+        li.textContent = listaAmigos[i];
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo(){ 
+    let resultado = document.getElementById('resultado');
+    if (listaAmigos.length > 0 || listaAmigos.length > 2 ){
+        let amigoSorteado = Math.floor(Math.random() * listaAmigos.length);
+        let li = document.createElement("li");
+        li.textContent = listaAmigos[amigoSorteado];
+        resultado.appendChild(li);
+    } else {
+        alert("Por favor, inserte al menos dos nombres.");
+    }
+}
+
+
 
